ca21d3889780bbb30785b88de90704a82e898d22
Weili An <China_Aisa@live.com>
2018-01-07 18:32:33 +0800

    Add time indicator

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,16 @@
 var MAX_POPULATION = 90
 var AIO_KEY = '75b6e2aa68bc48d9a65882c2c21b6a62'
+var ADAFRUIT_API = 'https://io.adafruit.com/api/v2/WilliamMTK/feeds/lib-population/data?limit=1&include=value,created_at'
 // Get last data through HTTP
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("lib-population").innerHTML =
-        JSON.parse(this.response)[0]['value']+'/'+MAX_POPULATION
+    	let tmp = JSON.parse(this.response)[0]
+        document.getElementById('lib-population').innerHTML = tmp['value']+'/'+MAX_POPULATION
+        document.getElementById('lib-time').innerHTML = (new Date(tmp['created_at'])).toLocaleString()
    }
 };
-xhttp.open('GET','https://io.adafruit.com/api/v2/WilliamMTK/feeds/lib-population/data?limit=1&include=value',false)
+xhttp.open('GET',ADAFRUIT_API,false)
 xhttp.setRequestHeader('X-AIO-Key',AIO_KEY)
 xhttp.send()
 
@@ -16,11 +18,14 @@ xhttp.send()
 var client = mqtt.connect('mqtts://io.adafruit.com',{username:'WilliamMTK', password:AIO_KEY})
 client.on('connect',function(){
     // Connect to MQTT server
-    client.subscribe('WilliamMTK/f/lib-population')
+    client.subscribe('WilliamMTK/f/lib-population/json')
     console.log('Connected to MQTT server, subscription completed')  
 })
 client.on('message',function(topic,msg){
-    document.getElementById('lib-population').innerHTML = msg.toString()+'/'+MAX_POPULATION
+	let tmp = JSON.parse(msg)
+	console.log(tmp)
+    document.getElementById('lib-population').innerHTML = tmp['last_value']+'/'+MAX_POPULATION
+    document.getElementById('lib-time').innerHTML = (new Date(tmp['updated_at'])).toLocaleString()
     client.end()
 })
 
